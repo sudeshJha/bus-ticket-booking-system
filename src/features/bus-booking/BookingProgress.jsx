27005d6fb96f38777da7873ff434a5750464201c
Sudesh Jha <sudeshjha2005@gmail.com>
2026-08-01 01:17:51 +0530

    add name in progress step

--- src/features/bus-booking/BookingProgress.jsx
+++ src/features/bus-booking/BookingProgress.jsx
@@ -11,17 +11,32 @@ const BookingProgress = ({ progressInfo }) => {
   const { progress, nextProgress, prevProgress, jumpProgress } = progressInfo;
 
   return (
-    <div className="mx-auto w-200 px-5 py-10 flex items-center justify-center">
-      {steps.map((step) => {
-        return (
-          <ProgressStep
-            key={step.id}
-            step={step}
-            progress={progress}
-            jumpProgress={jumpProgress}
-          />
-        );
-      })}
+    <div className="mx-auto w-200 py-10">
+      <div className="w-full flex items-center justify-center">
+        {steps.map((step) => {
+          return (
+            <>
+              <ProgressStep
+                key={step.id}
+                step={step}
+                progress={progress}
+                jumpProgress={jumpProgress}
+              />
+            </>
+          );
+        })}
+      </div>
+
+      <div className="w-210 mt-2 flex justify-between justify-self-center">
+        {steps.map((step) => (
+          <span
+            key={step.name}
+            className={`${step.id === progress ? "text-text-primary" : "text-text-secondary"} font-bold text-xl`}
+          >
+            {step.name}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
