ee3d8f63f2c659724c712a1df5ad19dd6a0ebc53
Sudesh Jha <sudeshjha2005@gmail.com>
2026-09-25 21:30:15 +0530

    query client provider in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,38 +15,51 @@ import SignupPage from "./pages/SignupPage";
 import BookingsPage from "./pages/BookingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import SettingsPage from "./pages/SettingsPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    // queries: { staleTime: 60 * 1000 },
+    queries: {
+      // the time in which the date in the cache will say relevant
+      staleTime: 0,
+    },
+  },
+});
 
 const App = () => {
   return (
     <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<IndexPage />} />
+      <QueryClientProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<IndexPage />} />
 
-            <Route element={<PassengerLayout />}>
-              <Route path="/home" element={<Home />} />
-              <Route path="/search_bus" element={<SearchBus />} />
-              <Route path="/search_bus/:id" element={<BusBooking />} />
+              <Route element={<PassengerLayout />}>
+                <Route path="/home" element={<Home />} />
+                <Route path="/search_bus" element={<SearchBus />} />
+                <Route path="/search_bus/:id" element={<BusBooking />} />
+              </Route>
+
+              <Route element={<OperatorLayout />}>
+                <Route path="dashboard" element={<Logo />} />
+                <Route path="my_company" element={<Footer />} />
+                {/* other links for operator */}
+              </Route>
             </Route>
 
-            <Route element={<OperatorLayout />}>
-              <Route path="dashboard" element={<Logo />} />
-              <Route path="my_company" element={<Footer />} />
-              {/* other links for operator */}
+            <Route path="/settings" element={<SettingsPage />}>
+              <Route index element={<Navigate replace to="profile" />} />
+              <Route path="profile" element={<ProfilePage />} />
+              <Route path="bookings" element={<BookingsPage />} />
             </Route>
-          </Route>
 
-          <Route path="/settings" element={<SettingsPage />}>
-            <Route index element={<Navigate replace to="profile" />} />
-            <Route path="profile" element={<ProfilePage />} />
-            <Route path="bookings" element={<BookingsPage />} />
-          </Route>
-
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
--- src/features/auth/useSignup.js
+++ src/features/auth/useSignup.js
@@ -1,11 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
 import { signup } from "../services/apiUser";
+import toast from "react-hot-toast";
 
 export function useSignup() {
   const { mutate: signup, isLoading: signingUp } = useMutation({
     mutationFn: (userData) => signup(userData),
 
-    onSuccess: () => {},
+    onSuccess: () => {
+      toast.success();
+    },
     onError: () => {},
   });
 }
